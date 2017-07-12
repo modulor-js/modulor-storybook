e7c4b2b578867fb29afe6186dba996953f8b5621
Alexander Zhukov <a.zhukov@holidaypirates.com>
2017-07-12 17:39:06 +0200

    check if in iframe

--- js/manager.js
+++ js/manager.js
@@ -2,5 +2,12 @@ const story = require('./story');
 
 console.log('application!!!');
 
+console.log(inIframe() ? 'iframe' : 'main', window.location.pathname);
+
 console.log(story.getStories());
+
+
+function inIframe(){
+  return window.self !== window.top;
+}
 
