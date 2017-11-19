001e1e6e64ee7f51ec566e5f109556492c5d13f4
Alexander Zhukov <a.zhukov@holidaypirates.com>
2017-11-19 23:50:10 +0100

    refactor(sandbox manager): move styles next to components

--- components/preview-frame/preview-frame.html.js
+++ components/preview-frame/preview-frame.html.js
@@ -7,6 +7,51 @@ module.exports = (scope) => `
       height: 100%;
       border: none;
     }
+
+    .fullscreen-icon,
+    .arrows-overlap {
+      background-color: Gainsboro;
+      border-radius: 3px;
+    }
+    .fullscreen-icon {
+      display: block;
+      font-style: normal;
+      height: 25px;
+      position: absolute;
+      right: 15px;
+      top: 15px;
+      width: 25px;
+    }
+    .fullscreen-icon:after,
+    .fullscreen-icon:before {
+      color: gray;
+      content: "\u2192";
+      display: block;
+      font-size: 18px;
+      position: absolute;
+    }
+    .fullscreen-icon:after {
+      transform: rotate(-45deg);
+      top: -3px;
+      right: -2px;
+    }
+    .fullscreen-icon:before {
+      transform: rotate(135deg);
+      bottom: -3px;
+      left: -2px;
+    }
+    .arrows-overlap {
+      display: inline-block;
+      background-color: gainsboro;
+      height: 8px;
+      width: 8px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-left: -4px;
+      margin-top: -4px;
+      z-index: 50;
+    }
   </style>
 
   <a id="fullscreen-anchor" class="fullscreen-icon" href="" target="_blank">
